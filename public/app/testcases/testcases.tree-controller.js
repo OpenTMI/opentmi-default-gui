8931795aec1c941e1cc395c1bc4d506a058d0a36
Jussi Vatjus-Anttila <jussiva@gmail.com>
2015-09-27 19:21:03 +0300

    fetch tc tree directly from backend

--- public/app/testcases/testcases.tree-controller.js
+++ public/app/testcases/testcases.tree-controller.js
@@ -8,64 +8,106 @@ angular.module('tmtControllers')
 
     $log.info('init TestcaseTreeController')
 
+    $scope.typesConfig = {
+      /*"default": {
+        "icon": "jstree-icon jstree-folder"
+      },*/
+      "tc": {
+        "icon": "jstree-icon jstree-file"
+      }
+    }
+    $scope.treeModel = [];
 
-    $scope.treeModel = [
-      
-        {
-          "id": "connetivity",
-          "parent": "#",
-          "text": "Connectivity"
-        },
-          {
-            "id": "6lowpan",
-            "parent": "connetivity",
-            "text": "6lowpan"
-          },{
-            "id": "thread",
-            "parent": "connetivity",
-            "text": "Thread"
-          },{
-            "id": "bluetooth",
-            "parent": "connetivity",
-            "text": "Bluetooth"
-          },
-        {
-          "id": "drivers",
-          "parent": "#",
-          "text": "Drivers"
-        },
-          {
-            "id": "uart",
-            "parent": "drivers",
-            "text": "uart"
-          },{
-            "id": "usb",
-            "parent": "drivers",
-            "text": "usb"
-          },
-      ];
 
-      Testcase.query({t: 'distinct', f: "tcid", q: {'other_info.components': '6lowpan'}}).$promise.then( 
-        function(lwpTcs){
-          _.each(lwpTcs, function(tcid){
-            $scope.treeModel.push({
-              id: tcid,
-              parent: '6lowpan',
-              text: tcid,
-              /*icon:"/jstree/imgs/tree.png"*/
-            });
-          });
+
+    var addNode = function(node){
+      $scope.treeModel.push(node);
+    }
+    var clearTree = function(){ $scope.treeModel = []; }
+
+    addNode({
+      id: 'bluetooth',
+      parent: '#',
+      text: 'Bluetooth'
+    });
+    
+
+    var ListOfTcs = []
+
+    var doRest = function(listOfTcs, component){
+      if( component ) {
+        Testcase.query({t: 'distinct', f: 'tcid', s: {'tcid': -1}, 
+            q: { 
+              'other_info.components': component, 
+              'tcid': { $nin: [ listOfTcs ] }
+            }
+        }).$promise.then( function(tcs){
+          _.each(tcs, doTc(component, ''));
         });
-      Testcase.query({t: 'distinct', f: "tcid", q: {'other_info.components': 'thread'}}).$promise.then( 
-        function(lwpTcs){
-          _.each(lwpTcs, function(tcid){
-            $scope.treeModel.push({
-              id: tcid,
-              parent: 'thread',
-              text: tcid
-            });
-          });
-        } 
-      );
+      } else {
+        addNode({
+          id: '.uncategory',
+          parent: '#',
+          text: 'Unknown'
+        });
+        Testcase.query({t: 'distinct', f: 'tcid', s: {'tcid': -1}, 
+            q: { 'tcid': { $nin: [ listOfTcs] } }
+        }).$promise.then( function(tcs){
+          _.each(tcs, doTc('', 'uncategory', true));
+        });
+      }
+    }
+
+    var doTc = function(component, feature, rest){
+      return function(tc){
+        if(!rest)ListOfTcs.push(tc);
+        var parent = component;
+        if( feature ) parent += '.'+feature;
+        addNode({
+          id: parent+'.'+tc,
+          parent: parent,
+          text: tc,
+          type: "tc"
+        });
+      }
+    }
+    var doFeature = function(component){
+      return function(feature) {
+        addNode({
+          id: component+'.'+feature,
+          parent: component,
+          text: feature,
+        });
+        Testcase.query({t: 'distinct', f: 'tcid', 
+          q: { 
+            $and: [ 
+              {'other_info.components': component}, 
+              {'other_info.features': feature} 
+            ]
+          }})
+        .$promise.then( function(tcs){
+          _.each(tcs, doTc(component, feature));
+          doRest(tcs, component);
+        });
+      }
+    }
+    var doComponent = function(component){
+      addNode({
+        id: component,
+        parent: '#',
+        text: component,
+      });
+      Testcase.query({t: 'distinct', f: "other_info.features", q: {'other_info.components': component}})
+      .$promise.then( function(features){
+        _.each(features, doFeature(component) );
+      });
+    }
+
+    Testcase.query({t: 'distinct', f: "other_info.components"}).$promise.then( 
+      function(components) {
+        _.each( components, doComponent );
+        doRest(ListOfTcs)
+      }
+    );
   }])
 ;

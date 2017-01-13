f25c81e8f3c637156a4378d67b325178be816111
Jussi Vatjus-Anttila <jussi.vatjus-anttila@arm.com>
2017-01-13 09:58:22 +0200

    add pagination for builds list view

--- public/app/builds/builds.list-controller.js
+++ public/app/builds/builds.list-controller.js
@@ -2,17 +2,17 @@
 
 angular.module('OpenTMIControllers')
   .controller('BuildListController', 
-             ['$scope', 'Builds', '$stateParams', '$log',
-    function ($scope,   Builds,   $stateParams,    $log) {
+             ['$scope', 'Builds', '$http', '$stateParams', '$log',
+    function ($scope,   Builds,    $http,   $stateParams,   $log) {
   
-    $log.info('init BuildListController')
+    $log.info('init BuildListController');
     
     
     var vcsLinkCellTemplate = '<div class="ngCellText" ng-class="col.colIndex()">' +
                        '<a href="{{ row.entity.vcs[0].url }}">{{ row.entity.vcs[0].branch }}</a>' +
                        '</div>';
     var ciLinkCellTemplate = '<div class="ngCellText" ng-class="col.colIndex()">' +
-                       '<a href="{{ row.entity.ci.location.url }}">{{ row.entity.ci.job.name}} # {{ row.entity.ci.job.number}}</a>' +
+                       '<a href="{{ row.entity.ci.location.url }}">{{ row.entity.ci.job.name}} PR#{{ row.entity.ci.job.number}}</a>' +
                        '</div>';
     var nameTemplate = '<div class="ngCellText" ng-class="col.colIndex()">' +
                        '<a href="#/duts/builds/{{ row.entity._id }}">{{ row.entity[col.field] }}</a>' +
@@ -20,18 +20,20 @@ angular.module('OpenTMIControllers')
     var dlCellTemplate = '<div class="ngCellText" ng-class="col.colIndex()">' +
                        '<a target="new" href="api/v0/duts/builds/{{ row.entity._id }}/files/0/download">{{ COL_FIELD }} ({{(row.entity.files[0].size/1024).toFixed(0)}} kB)</a>' +
                        '</div>';
+    //var toolchainCellTemplate = '<div class="ui-grid-cell-contents"><span>{{_.find(["gcc","armcc","iar"], function(s){return s.match(COL_FIELD.toLower()}}</span></div>';
+
     var defaultCellTemplate = '<div class="ui-grid-cell-contents"><span>{{COL_FIELD}}</span></div>';
 
     $scope.columns = [
       { field: 'cre.time', width:200, displayName: 'Date' },
-      { field: 'name', width:400, cellTemplate: nameTemplate, displayName: 'Name' },
+      { field: 'name', width:350, cellTemplate: nameTemplate, displayName: 'Name' },
       { field: 'target.os', width:100, displayName: 'OS' },
-      { field: 'target.hw.model', width:150, displayName: 'Target' },
+      { field: 'target.hw.model', width:200, displayName: 'Target' },
       { field: 'vcs[0].branch', width:100, cellTemplate: vcsLinkCellTemplate, displayName: 'PR' },
-      { field: 'ci[0].url', width:100, cellTemplate: ciLinkCellTemplate, displayName: 'CI Job' },
+      { field: 'ci[0].url', width:300, cellTemplate: ciLinkCellTemplate, displayName: 'CI Job' },
       { field: 'configuration.name', width:150, displayName: 'Configuration' },
-      { field: 'configuration.toolchain.name', width:100, displayName: 'Toolchain' },
-      { field: 'files[0].name', cellTemplate: dlCellTemplate, displayName: 'File', cellTooltip: function (row, col) {return row.entity.name}  },
+      { field: 'configuration.toolchain.name', width:160, /*cellTemplate: toolchainCellTemplate,*/ displayName: 'Toolchain' },
+      { field: 'files[0].name', cellTemplate: dlCellTemplate, displayName: 'File', cellTooltip: function (row, col) {return row.entity.name} }
     ];
     $scope.gridOptions = { 
       columnDefs: $scope.columns,
@@ -43,12 +45,42 @@ angular.module('OpenTMIControllers')
       exporterMenuCsv: true,
       enableGridMenu: true
     };
+    $scope.pagination = {
+        pageSize: 100,
+        pageNumber: 0,
+        totalItems: null,
+        getTotalPages: function () {
+            return Math.ceil(this.totalItems / this.pageSize);
+        },
+        nextPage: function () {
+            if (this.pageNumber < this.getTotalPages()) {
+                this.pageNumber++;
+                $scope.load();
+            }
+        },
+        previousPage: function () {
+            if (this.pageNumber > 0) {
+                this.pageNumber--;
+                $scope.load();
+            }
+        }
+    };
+
+    $http.get('/api/v0/duts/builds?t=count').success(function(response) {
+        $scope.pagination.totalItems = response.count;
+    });
     
-    function doUpdateList(q)
+    $scope.load = function(q)
     {
       if(!q) q = {};
-      Builds.query({q: JSON.stringify(q), f: "_id name target files configuration cre.time vcs ci"})
-        .$promise.then( function(builds){
+      let req = {
+          q: JSON.stringify(q),
+          f: "_id name target files configuration cre.time vcs ci",
+          s: {'cre.time': -1},
+          sk: $scope.pagination.pageSize*$scope.pagination.pageNumber,
+          l: $scope.pagination.pageSize
+      };
+      Builds.query(req).$promise.then( function(builds){
           $log.info(builds);
           $scope.dataBuilds = builds;
           $scope.$root.$broadcast('buildListStatus', status);
@@ -56,18 +88,7 @@ angular.module('OpenTMIControllers')
     }
     
     $scope.gridOptions.data = 'dataBuilds';
-    doUpdateList();
-    /*
-    $scope.$on('tcFilter', function(event, data) {
-      
-      var q = {}
-      data.tags.forEach( function(tag){
-        if( !q.$or ) q.$or = [];
-        q.$and.push( {target: {"$regex": ("/"+tag+"/"), "$options":"i"}} );
-      });
-      doUpdateList(q);
-      
-    });*/
+    $scope.load();
     
     $scope.gridOptions.onRegisterApi = function(gridApi){
       //set gridApi on scope
@@ -75,22 +96,5 @@ angular.module('OpenTMIControllers')
     }; 
     
   }])
-  /*
-  .filter('mapStatus', function() {
-    var statusHash = {
-      'released': 'released',
-      'develop': 'develop',
-      'maintenance': 'maintenance',
-      'unknown': 'unknown'
-    };
-    
-    return function(input) {
-      if (!input){
-        return '';
-      } else {
-        return statusHash[input];
-      }
-    };
-  })*/
   
   ;

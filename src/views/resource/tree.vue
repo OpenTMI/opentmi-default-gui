58b4d03444f8d87345f495c0f689662643bb33f5
Jussi Vatjus-Anttila <jussiva@gmail.com>
2022-03-24 11:49:18 +0200

    improve resources list and tree views (#132)

--- src/views/resource/tree.vue
+++ src/views/resource/tree.vue
@@ -1,75 +1,117 @@
 <template>
   <div class="app-container">
-    <v-jstree :data="treeData" :async="loadData" @item-click="itemClick" />
+    <el-row>
+      <el-col :span="12">
+        <v-jstree :data="treeData" :async="loadData" @item-click="itemClick" />
+      </el-col>
+      <el-col :span="12">
+        <vue-json-pretty
+          :data="row"
+          :deep="1"
+          :deep-collapse-children="true"
+          :show-length="true"
+          :show-double-quotes="false"
+        />
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
 import VJstree from 'vue-jstree'
-import { resourceList } from '@/api/resources'
+import VueJsonPretty from 'vue-json-pretty'
+import { resourceList, searchResource } from '@/api/resources'
 
 export default {
   name: 'ResultTree',
   components: {
-    VJstree
+    VJstree,
+    VueJsonPretty
   },
   data() {
     return {
+      row: {},
       treeData: [],
       loadData: this._.debounce(this._loadData, 250)
     }
   },
   methods: {
-    _query(filters) {
-      const query = { f: `_id name` }
+    distinct(filters, field) {
+      const query = {
+        t: 'distinct',
+        f: field,
+        l: 1000,
+        to: 5000
+      }
       return this._.merge(query, filters)
     },
     _loadData(node, resolve) {
       const id = node.data.id ? node.data.id : 0
       const { level, type, filters = {}} = node.data
+      const options = { level: level + 1, field: NaN, type, filters }
       if (id === 0) {
         this.getRoot().then(resolve)
-      } else if (type === 'rooms') {
-        if (level === 0) { // room
-          const options = { level: 1, type, filters: { type: 'room' }}
-          this.getDistinct(options).then(resolve)
-        } else if (level === 1) { // system
-          const options = { level: 2, type, filters }
-          this.getDistinct(options).then(resolve)
-        } else if (level === 2) { // computer
-          const options = { level: 3, type, filters }
-          return this.getDistinct(options).then(resolve)
+      } else if (type === 'location') {
+        if (level === 0) {
+          options.field = 'location.site'
+        } else if (level === 1) {
+          options.field = 'location.room'
+        } else if (level === 2) {
+          options.field = 'location.rack'
+        } else if (level === 3) {
+          options.field = 'location.bed'
+        } else if (level === 4) {
+          options.field = 'location.slot'
         } else {
-          const icon = undefined // 'fa fa-check icon-state-success'
-          const isLeaf = true
-          const options = { level: 4, type, filters, isLeaf, icon }
-          return this.getDistinct(options).then(resolve)
+          options.icon = 'el-icon-tickets'
+          options.isLeaf = true
+          options.field = '_id'
+        }
+      } else if (type === 'type') {
+        if (level === 0) {
+          options.field = 'type'
+        } else if (level === 1) {
+          options.field = 'item.model'
+        } else {
+          options.isLeaf = true
+          options.icon = 'el-icon-tickets'
+          options.field = '_id'
+        }
+      } else if (type === 'model') {
+        if (level === 0) {
+          options.field = 'item.model'
+        } else {
+          options.isLeaf = true
+          options.icon = 'el-icon-tickets'
+          options.field = '_id'
         }
       } else {
-        resolve([])
+        return resolve([])
       }
+
+      return this.getDistinct(options).then(resolve)
     },
     getRoot() {
       return Promise.resolve([
-        { text: 'rooms', level: 0, type: 'rooms', isLeaf: false }
-        // { text: 'system', level: 0, type: 'systems', isLeaf: false },
-        // { text: 'dut', level: 0, type: 'duts', isLeaf: false }
+        { text: 'by location', level: 0, type: 'location', isLeaf: false },
+        { text: 'by type', level: 0, type: 'type', isLeaf: false },
+        { text: 'by model', level: 0, type: 'model', isLeaf: false }
       ])
     },
-    getDistinct({ level, filters = {}, type, isLeaf = false, icon }) {
-      const query = this._query(filters)
+    getDistinct({ level, field, filters = {}, type, isLeaf = false, icon }) {
+      const query = this.distinct(filters, field)
       return resourceList(query)
         .then(({ data }) => {
-          const mapper = obj => {
-            const text = obj.name
-            const filters = this._.merge(filters, { parent: obj._id })
+          const mapper = text => {
+            const _filters = this._.clone(filters)
+            this._.merge(_filters, { [field]: text })
             return {
               text,
               level,
               isLeaf,
               icon,
               type,
-              filters
+              filters: _filters
             }
           }
           const treeData = this._.map(data, mapper)
@@ -77,8 +119,21 @@ export default {
         })
     },
     itemClick(node, item, e) {
-      // eslint-disable-next-line no-console
-      console.log(node.model.text + ' clicked !', item, e)
+      const filters = this._.cloneDeep(node.model.filters)
+
+      if (node.model.isLeaf) {
+        const _id = node.model.filters._id
+        console.log('Get result: ', _id)
+        searchResource(_id).then(({ data }) => {
+          // eslint-disable-next-line no-console
+          console.log(`searchResult(${_id}): ${JSON.stringify(data)}`)
+          this._.unset(data, '__v')
+          this._.unset(data, '_id')
+          this.row = data
+        })
+      } else {
+        this.row = filters
+      }
     }
   }
 

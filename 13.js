bcc88b005e14f57dfb3be9aaa1369903441e0894
jupe <jupe@users.noreply.github.com>
2022-03-24 09:17:30 +0000

    deploy: fd733ed46b3813fd885737a9bec86b0624bac0a2

--- 13.js
+++ 13.js
@@ -522,28 +522,211 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "searchResource", function() { return searchResource; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "resourceList", function() { return resourceList; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "getSchema", function() { return getSchema; });
-/* harmony import */ var _utils_request__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @/utils/request */ "./src/utils/request.js");
+/* harmony import */ var core_js_modules_web_dom_iterable__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! core-js/modules/web.dom.iterable */ "./node_modules/core-js/modules/web.dom.iterable.js");
+/* harmony import */ var core_js_modules_web_dom_iterable__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_web_dom_iterable__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var core_js_modules_es6_regexp_split__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! core-js/modules/es6.regexp.split */ "./node_modules/core-js/modules/es6.regexp.split.js");
+/* harmony import */ var core_js_modules_es6_regexp_split__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_es6_regexp_split__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _utils_request__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @/utils/request */ "./src/utils/request.js");
+
+
 
 function searchResource(id) {
-  return Object(_utils_request__WEBPACK_IMPORTED_MODULE_0__["default"])({
+  return Object(_utils_request__WEBPACK_IMPORTED_MODULE_2__["default"])({
     url: "/api/v0/resources/".concat(id),
     method: 'get'
   });
 }
 function resourceList(query) {
-  return Object(_utils_request__WEBPACK_IMPORTED_MODULE_0__["default"])({
+  return Object(_utils_request__WEBPACK_IMPORTED_MODULE_2__["default"])({
     url: '/api/v0/resources',
     method: 'get',
     params: query
   });
 }
 function getSchema(_) {
-  return Object(_utils_request__WEBPACK_IMPORTED_MODULE_0__["default"])({
+  return Object(_utils_request__WEBPACK_IMPORTED_MODULE_2__["default"])({
     url: "/api/v0/schemas/Resource.json",
     method: 'get'
   }).then(function (_ref) {
     var data = _ref.data;
     var schema = data.schema;
+
+    var fill = function fill(key, _ref2) {
+      var title = _ref2.title,
+          _ref2$def = _ref2.def,
+          def = _ref2$def === void 0 ? '' : _ref2$def,
+          type = _ref2.type;
+      key = key.split('.').join('.properties.');
+
+      _.set(schema, "properties.".concat(key, ".title"), title);
+
+      if (def) _.set(schema, "properties.".concat(key, ".default"), def);
+      if (type) _.set(schema, "properties.".concat(key, ".type"), type);
+    };
+
+    var drop = function drop(key) {
+      key = key.split('.').join('.properties.');
+
+      _.unset(schema, "properties.".concat(key));
+    };
+
+    var dropKeys = function dropKeys(keys) {
+      keys.forEach(drop);
+    };
+
+    fill('name', {
+      title: 'Name'
+    });
+    fill('type', {
+      title: 'Type'
+    });
+    fill('item.model', {
+      title: 'Model'
+    });
+    fill('status.value', {
+      title: 'Status'
+    });
+    fill('status.note', {
+      title: 'Note'
+    });
+    fill('status.availability', {
+      title: 'Availability'
+    });
+    fill('hw.firmware.name', {
+      title: 'Name'
+    });
+    fill('hw.firmware.version', {
+      title: 'Version'
+    });
+    fill('hw.sn', {
+      title: 'Serial Number'
+    });
+    fill('hw.imei', {
+      title: 'Imei'
+    });
+    fill('hw.hwid', {
+      title: 'HW ID'
+    });
+    fill('ownership.corporation', {
+      title: 'Owner corporation'
+    });
+    fill('ownership.unit', {
+      title: 'unit'
+    });
+    fill('ownership.division', {
+      title: 'division'
+    });
+    fill('ownership.department', {
+      title: 'department'
+    });
+    fill('ownership.cost_center', {
+      title: 'cost center'
+    });
+    fill('ownership.author', {
+      title: 'author'
+    });
+    fill('ownership.purchased.timestamp', {
+      title: 'Purchaced time'
+    });
+    fill('ownership.purchased.note', {
+      title: 'Purchaced note'
+    });
+    fill('user_info.corporation', {
+      title: 'corporation'
+    });
+    fill('user_info.unit', {
+      title: 'unit'
+    });
+    fill('user_info.division', {
+      title: 'division'
+    });
+    fill('user_info.department', {
+      title: 'department'
+    });
+    fill('user_info.author', {
+      title: 'author'
+    });
+    fill('user_info.cost_center', {
+      title: 'cost_center'
+    });
+    fill('usage.type', {
+      title: 'Type'
+    });
+    fill('usage.group', {
+      title: 'Group'
+    });
+    fill('usage.automation.system', {
+      title: 'Automation System'
+    });
+    fill('network.hostname', {
+      title: 'Hostname'
+    });
+    fill('network.domain', {
+      title: 'domain'
+    });
+    fill('network.remote_connection.protocol', {
+      title: 'Protocol'
+    });
+    fill('network.remote_connection.url', {
+      title: 'Url'
+    });
+    fill('network.remote_connection.authentication.username', {
+      title: 'Username'
+    });
+    fill('network.remote_connection.authentication.password', {
+      title: 'Password'
+    });
+    fill('location.site', {
+      title: 'Site'
+    });
+    fill('location.country', {
+      title: 'country'
+    });
+    fill('location.city', {
+      title: 'city'
+    });
+    fill('location.adddress', {
+      title: 'adddress'
+    });
+    fill('location.postcode', {
+      title: 'postcode'
+    });
+    fill('location.room', {
+      title: 'room'
+    });
+    fill('location.subRoom', {
+      title: 'subRoom'
+    });
+    fill('location.geo', {
+      title: 'geo'
+    });
+    fill('location.rack', {
+      title: 'Rack'
+    });
+    fill('location.bed', {
+      title: 'Test Bed'
+    });
+    fill('location.slot', {
+      title: 'Slot'
+    });
+    fill('shield.rf', {
+      title: 'RF shielded',
+      type: 'boolean'
+    });
+    fill('childs', {
+      title: 'childs'
+    });
+    fill('parent', {
+      title: 'parent',
+      type: 'boolean'
+    });
+    fill('installed.os.name', {
+      title: 'OS'
+    });
+    fill('installed.os.build', {
+      title: 'Build _id'
+    });
+    dropKeys(['_id', 'cre', 'status.time', 'mod', 'item.ref', 'ownership.purchased.user']);
     return schema;
   });
 }
